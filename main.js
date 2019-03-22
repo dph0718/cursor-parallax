74b1327d775a6a9ba1333c6b1f80bf8d14c4f0a3
David <dph0718@gmail.com>
2019-03-21 19:32:38 -0600

    Adds some trig functions and displays calculated data

--- main.js
+++ main.js
@@ -1,53 +1,69 @@
-let midVerticalPx = function () {
-    return $(document).height() / 2;
-};
 
-let midHorizontalPx = function () {
-    return $(document).width() / 2;
-};
-
+console.log(`INITIAL HEIGHT: ${$(document).height()}`);
+const pi = Math.PI;
 
 $(document).on("mousemove", function (event) {
-    $("#log").text("pageX: " + event.pageX + ", pageY: " + event.pageY);
-    let centerY = midVerticalPx();
-    let centerX = midHorizontalPx();
     let pageHeight = $(document).height();
     let pageWidth = $(document).width();
+    let centerY = pageHeight / 2;
+    let centerX = pageWidth / 2;
+    let mouseX = event.pageX;
+    let mouseY = event.pageY;
+    let xDif = mouseX - centerX;
+    let yDif = mouseY - centerY;
+
+    let thetaMouse = Math.atan2(yDif, xDif) * 180 / Math.PI;
+
     let maxHeight = 30;
-    let maxWidth = 30;
+    let maxWidth = 20;
     let faceHeight = $("#block-face").height();
-    $("#faceHeight").text(`faceheight: ${faceHeight}`)
+    let faceWidth = $("#block-face").width();
+    //Skew and height formulas for right face
+    let rightWidth = Math.cos(thetaMouse / 180 * pi) * maxWidth;
+    let rightSkewY = 180 - thetaMouse * -1;
+    let offsetY = Math.tan(rightSkewY / 180 * pi) * 0.5 * rightWidth;
 
-    let bottomHeight = -maxHeight * ((event.pageY - centerY) / (pageHeight - centerY));
+    let leftWidth = -rightWidth;
+    let leftSkewY = rightSkewY;
+
+
+    // Skew and height formulas for bottom face.
+    let bottomHeight = Math.sin(thetaMouse / 180 * pi) * maxHeight;
+    let bottomSkewX = (90 - thetaMouse);
+    let offsetX = Math.tan(bottomSkewX / 180 * pi) * 0.5 * bottomHeight - (rightWidth/2) + (leftWidth/2);
+    // Skew and height forulas for top face.
     let topHeight = -bottomHeight;
-    let leftWidth = maxWidth * ((event.pageX - centerX) / (pageWidth - centerX));
-    let rightWidth = -leftWidth;
-
-    let skewX = -30 * ((event.pageX - centerX) / (pageWidth - centerX));
-    let skewY = -60 * ((event.pageY - centerY) / (pageHeight - centerY));
+    let topSkewX = bottomSkewX;
 
 
-    let offsetX = (bottomHeight / 2) * Math.tan(skewX * Math.PI / 180);
-    let offsetY = (rightWidth / 2) * Math.tan(skewY * Math.PI / 180);
+
+    $("#faceHeight").text(`face dimensions: ${faceWidth} x ${faceHeight}`)
+    $("#log").empty();
+    $("<h4>").text(`thetaMouse: ${thetaMouse}`).appendTo("#log");
+    $("<h4>").text(`sin of thetaMouse: ${Math.sin(thetaMouse / 180 * pi)}`).appendTo("#log");
+    $("<h4>").text(`offset: ${Math.tan(bottomSkewX / 180 * pi) * 0.5 * bottomHeight}`).appendTo("#log");
+    $("<h4>").text(`rightWidth: ${rightWidth}`).appendTo("#log");
+
+    $("<h4>").text(`thetaMouse: ${(thetaMouse / 180)} pi Radians`).appendTo("#log");
 
 
-    $("#block-bottom")
-        // .text(`${bottomHeight}px`)
-        .css('height', bottomHeight)
-        .css('transform', `skewX(${skewX}deg) translate(${offsetX}px, 0)`)
+
     $("#block-top")
-        // .text(`${bottomHeight}px`)
         .css('height', topHeight)
-        .css('transform', `skewX(${-skewX}deg) translate(${-offsetX}px, 0)`)
-    $("#block-right")
-        .css('width', rightWidth)
-        .css('transform', `skewY(${  skewY}deg) translate(0 , ${offsetY}px)`)
+        .css('transform', `skewX(${topSkewX}deg) translate(${offsetX}px, 0)`)
     $("#block-left")
         .css('width', leftWidth)
-        .css('transform', `skewY(${-skewY}deg) translate(0 , ${-offsetY}px)`)
-    $("#bottom-skew").text("Bottom skew: " + skewX + "deg");
+        .css('transform', `skewY(${leftSkewY}deg) translate(0 , ${offsetY}px)`)
+
+    $("#block-bottom")
+        .css('height', bottomHeight)
+        .css('transform', `skewX(${bottomSkewX}deg) translate(${offsetX}px, 0)`)
+    $("#block-right")
+        .css('width', rightWidth)
+        .css('transform', `skewY(${rightSkewY}deg) translate(0 , ${offsetY}px)`)
+    // $("#bottom-skew").text("Bottom skew: " + skewX + "deg");
 
 })
-    .click(function () { console.log(event.pageX); console.log(midVerticalPx()) });
+    .click(function () { console.log(event.pageX) });
 
 
